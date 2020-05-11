c6e8fa6e5152435336289cfbc9e84917e2aed0d1
Jiajun <398160658@qq.com>
2020-05-11 12:23:55 +0800

    [JiajunLi]refactor: finished the first test in Stringreversertest

--- src/main/java/com/twc/javaBasic/Exercise/stringReverser/StringReverser.java
+++ src/main/java/com/twc/javaBasic/Exercise/stringReverser/StringReverser.java
@@ -2,13 +2,15 @@ package com.twc.javaBasic.Exercise.stringReverser;
 
 public class StringReverser {
     public static String[] reverse(String input) {
-        // TODO: please implement the method to pass all the tests.
-        //  If you find it difficult, you can check the following documentation:
-        //  https://docs.oracle.com/javase/8/docs/api/java/util/StringTokenizer.html
-        //  Note that there are so many ways to solve this problem so you are not forced
-        //  to use certain class. The documentation is just a suggestion.
-        // <--start
-        return null;
-        // --end-->
+
+        String[] modifiedinput = input.split(" ");
+
+        for (int start = 0, end = modifiedinput.length - 1; start < end; start++, end--) {
+            String temp = modifiedinput[end];
+            modifiedinput[end] = modifiedinput[start];
+            modifiedinput[start] = temp;
+        }
+
+        return modifiedinput;
     }
 }
