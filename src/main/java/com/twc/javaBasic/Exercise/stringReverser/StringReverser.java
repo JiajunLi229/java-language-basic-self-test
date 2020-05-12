81452c1f599e92c92df3568413c8debecd9863f2
Jiajun <398160658@qq.com>
2020-05-12 12:24:36 +0800

    [JiajunLi]fix: stringreverser. test: finished stringreverser part

--- src/main/java/com/twc/javaBasic/Exercise/stringReverser/StringReverser.java
+++ src/main/java/com/twc/javaBasic/Exercise/stringReverser/StringReverser.java
@@ -2,7 +2,13 @@ package com.twc.javaBasic.Exercise.stringReverser;
 
 public class StringReverser {
     public static String[] reverse(String input) {
-
+        if (input == null) {
+            throw new IllegalArgumentException("input cannot be null!");
+        }
+        if (( (input == "") | (input == "   ") )){
+            String[] modifiedinput = new String[0];
+            return modifiedinput;
+        }
         String[] modifiedinput = input.split(" ");
 
         for (int start = 0, end = modifiedinput.length - 1; start < end; start++, end--) {
